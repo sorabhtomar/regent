dd7b5028936fbae03f5611848f31214359cb72c8
Garuganto <stephenkhouri@Traviss-Mac-Pro.local>
2013-10-25 15:51:12 -0400

    Add comments to ending tags

--- footer.php
+++ footer.php
@@ -11,11 +11,11 @@
 		<div class="widget-container">
 			<?php dynamic_sidebar( 'footer-right-widgets' ); ?>
 		</div>
-	</div>
+	</div><!-- .footer-content -->
 	<div id="copyright">
 		<p class="clear grid">Customizable WordPress themes by Royalty Themes - Proudly powered by WordPress</p>
 	</div>
-</footer> 
+</footer><!-- #main-footer -->
 
 <?php wp_footer(); ?>
 </body>
