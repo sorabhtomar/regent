41d7e3ecd105a80c9fc318274935d5e4ee792ab2
Garuganto <stephenkhouri@Traviss-Mac-Pro.local>
2013-10-25 15:52:23 -0400

    Various changes, see extended
    - Include control and output
    - Addition of css generator
    - Addition of custom comment code

--- functions.php
+++ functions.php
@@ -1,5 +1,26 @@
 <?php
 
+// Control and Output for Customizer
+get_template_part( 'inc/customizer/control' );
+get_template_part( 'inc/customizer/output' );
+
+// CSS generator function for output
+function generate_css( $selector, $style, $mod_name, $prefix='', $postfix='', $echo=true ) {
+      $return = '';
+      $mod = get_option($mod_name);
+      if ( ! empty( $mod ) ) {
+         $return = sprintf('%s { %s:%s; }',
+            $selector,
+            $style,
+            $prefix.$mod.$postfix
+         );
+         if ( $echo ) {
+            echo $return . "\r\n";
+         }
+      }
+      return $return;
+}
+
 // Enqueue scripts
 function theme_scripts() {
 	wp_deregister_script( 'jquery' );       
@@ -61,49 +82,49 @@ add_action( 'init', 'register_my_menus' );
 }
 // Custom comments loop
 function regent_comment($comment, $args, $depth) {
-		$GLOBALS['comment'] = $comment;
-		extract($args, EXTR_SKIP);
+	$GLOBALS['comment'] = $comment;
+	extract($args, EXTR_SKIP);
 
-		if ( 'div' == $args['style'] ) {
-			$tag = 'div';
-			$add_below = 'comment';
-		} else {
-			$tag = 'li';
-			$add_below = 'div-comment';
-		}
+	if ( 'div' == $args['style'] ) {
+		$tag = 'div';
+		$add_below = 'comment';
+	} else {
+		$tag = 'li';
+		$add_below = 'div-comment';
+	}
 ?>
-		<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
-		<?php if ( 'div' != $args['style'] ) : ?>
-		<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
-		<?php endif; ?>
-		<div class="comment-table">
-			<div class="comment-author vcard">
-			<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-			<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?>
+	<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
+	<?php if ( 'div' != $args['style'] ) : ?>
+	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+	<?php endif; ?>
+	<div class="comment-table">
+		<div class="comment-author vcard">
+		<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+		<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?>
+		</div>
+		<div class="spacer"></div>
+		<div class="comment-box">
+			<div class="comment-triangle"></div>
+			<?php if ($comment->comment_approved == '0') : ?>
+					<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
+					<br />
+			<?php endif; ?>
+			<div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
+				<?php
+					/* translators: 1: date, 2: time */
+					printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
+				?>
 			</div>
-			<div class="spacer"></div>
-			<div class="comment-box">
-				<div class="comment-triangle"></div>
-				<?php if ($comment->comment_approved == '0') : ?>
-						<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
-						<br />
-				<?php endif; ?>
-				<div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
-					<?php
-						/* translators: 1: date, 2: time */
-						printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
-					?>
-				</div>
 
-				<?php comment_text() ?>
-			</div><!-- .comment-box -->
-		</div><!-- .clear -->
+			<?php comment_text() ?>
+		</div><!-- .comment-box -->
+	</div><!-- .clear -->
 
-		<div class="reply">
-		<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-		</div>
-		<?php if ( 'div' != $args['style'] ) : ?>
-		</div>
-		<?php endif; ?>
+	<div class="reply">
+	<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+	</div>
+	<?php if ( 'div' != $args['style'] ) : ?>
+	</div>
+	<?php endif; ?>
 <?php
-        }
+    }
