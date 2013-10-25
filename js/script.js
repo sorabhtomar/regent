43356c90cf938611f0c64be95a3163b09e2a69d3
Garuganto <stephenkhouri@Traviss-Mac-Pro.local>
2013-10-25 15:54:05 -0400

    Various changes, see extended
    - Code to add class to parent li element of sub menu
    - Addition of hoverIntent function added to main-menu
    - Code to append raquo to parent li element

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
  $(window).load(function() {
 	// suckerfish
 	sfHover = function() {
-		var sfEls = document.getElementById("main-menu").getElementsByTagName("LI");
+		var sfEls = document.getElementById("main-menu").getElementsByTagName("li");
 		for (var i=0; i<sfEls.length; i++) {
 			sfEls[i].onmouseover=function() {
 				this.className+=" sfhover";
@@ -11,9 +11,28 @@
 			}
 		}
 	}
+	
+	$("nav#main-menu .sub-menu").hover(
+		function(){ // Mouse Over
+			$(this).parent().addClass("parent-li");
+		},
+		function(){ // Mouse Out
+			$(this).parent().removeClass("parent-li");
+		}
+	);
 
+	//drop down menu slidedown
+	$('nav#main-menu ul li').hoverIntent(
+		function () {
+			$('ul', this).stop(false,true).show();
+		}, 
+		function () {
+			$('ul', this).stop(false,true).hide();          
+		}
+    );
 
-
+    //drop down arrow indicator for sub navigation
+	$('nav#main-menu li ul ul').parent('li').children('a').append('<span class="raquo">&raquo;</span> ');
 
 	$("h1#mobile-header").click(function(){
 	  $("nav#main-menu").toggle();
